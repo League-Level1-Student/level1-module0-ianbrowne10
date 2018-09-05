e39f230f71c875e41109ad7672a280ba96958a05
nicholaskimsd <league@WTS5.attlocal.net>
2018-09-04 18:26:57 -0700

    Intro to Methods (IP)

---
+++ src/IntroToMethods.java
@@ -0,0 +1,18 @@
+import javax.swing.JOptionPane;
+
+public class IntroToMethods {
+public static void main(String[] args) {
+	weRock();
+rocker();
+}
+
+static void weRock() {
+	JOptionPane.showMessageDialog(null, "The League Rocks!");
+}
+static void rocker() {
+	JOptionPane.showMessageDialog(null, "The Tate Rocks!");
+}
+
+
+
+}
