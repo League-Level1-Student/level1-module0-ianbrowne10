8cf52705e53ad79514038729a53c27e266b754c6
Lucas Baizer <titaniumsapphire32@gmail.com>
2018-09-18 18:22:34 -0700

    Intro to Methods

--- src/IntroToMethods.java
+++ src/IntroToMethods.java
@@ -1,38 +1,66 @@
 import javax.swing.JOptionPane;
+import javax.swing.plaf.synth.SynthSeparatorUI;
 
 import org.omg.Messaging.SyncScopeHelper;
 
 public class IntroToMethods {
-public static void main(String[] args) {
-new IntroToMethods().runMethods();
+	public static void main(String[] args) {
+		new IntroToMethods().runMethods();
 
-}
-void runMethods() {
-	weRock("Ia");
-	rocker("n ");
-truth("su");
-Truth("ck");
-fartEcho("s a");
-repeat("lot");
+	}
+
+	void runMethods() {
+		weRock();
+		rocker("billy the jack in the box employee");
+		truth("jeffry the man from the burger king commercial");
+		Echo("moooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo    ");
+		repeat("lot");
+		int sum = add(10, 20);
+		System.out.println(sum);
+num(10);
+	}
+
+	void weRock() {
+		JOptionPane.showMessageDialog(null, "The League Rocks!");
+	}
+
+	void rocker(String rock) {
+		JOptionPane.showMessageDialog(null, rock + " rocks!");
+	}
+
+	void truth(String truth) {
+		if (truth.equalsIgnoreCase("jeffry the man from the burger king commercial")) {
+			JOptionPane.showMessageDialog(null, truth + " " + "is awesome!");
+		} else {
+			JOptionPane.showMessageDialog(null, truth + " sucks!");
+		}
+	}
+
+	void Echo(String fart) {
+		JOptionPane.showMessageDialog(null, fart + fart);
+
+	}
+
+	void repeat(String repeat) {
+		String ans = JOptionPane.showInputDialog("Enter a Whole Number");
+		int num = Integer.parseInt(ans);
+		for (int i = 0; i < num; i++) {
+			System.out.println(repeat + " ");
+		}
+	}
+
+int add(int num1, int num2 ) {
+return num1 + num2;
 }
 
- void weRock(String we) {
-	JOptionPane.showMessageDialog(null, we);
-}
- void rocker(String rock) {
-	JOptionPane.showMessageDialog(null, rock);
-}
- void truth(String truth) {
-	JOptionPane.showMessageDialog(null, truth);
-}
- void Truth(String truth) {
-	JOptionPane.showMessageDialog(null, truth);
-}
- void fartEcho(String fart) {
-	JOptionPane.showMessageDialog(null, fart);
+ void num (int num) {
 
-}
-void repeat(String repeat) {
-JOptionPane.showMessageDialog(null, repeat);
-}
-}
+	 if(num%2==1)
+
+	{
+		System.out.println("It's odd!");
+	}else
+	{
+		System.out.println("It's even");
+	}
+}}
